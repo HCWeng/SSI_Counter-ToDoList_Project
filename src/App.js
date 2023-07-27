cc907c71bbe779271caa38e91913c99f876de25b
Andrew Weng <78281843+HCWeng@users.noreply.github.com>
2023-07-27 06:04:36 +0000

    update handler

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./styles.css";
-//import TdList from "./components/TdList";
 import ContentContainer from "./components/ContentContainer";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      {/* <TdList /> */}
+      <h2>Start editing to see some magic happen!</h2>      
       <ContentContainer />
     </div>
   );
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-const Counter = () => {
+function Counter() {
   const [count, setCount] = useState(0);
+  const [timer, setTimer] = useState(null);
+  const [timerRunning, setTimerRunning] = useState(false);
 
-  const handleIncrement = () => {
+  useEffect(() => {
+    // Cleanup the timer when component unmounts
+    return () => clearInterval(timer);
+  }, [timer]);
+
+  const incrementHandler = () => {
     setCount((prevCount) => prevCount + 1);
   };
 
-  const handleDecrement = () => {
+  const decrementHandler = () => {
     setCount((prevCount) => prevCount - 1);
   };
 
+  const incrementIfOddHandler = () => {
+    if (count % 2 !== 0) {
+      setCount((prevCount) => prevCount + 1);
+    }
+  };
+
+  const asyncIncrementHandler = () => {
+    setTimeout(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+  };
+
+  const toggleTimerHandler = () => {
+    if (timerRunning) {
+      clearInterval(timer);
+      setTimerRunning(false);
+    } else {
+      const newTimer = setInterval(() => {
+        setCount((prevCount) => prevCount + 1);
+      }, 1000);
+      setTimer(newTimer);
+      setTimerRunning(true);
+    }
+  };
+
   return (
-    <div>
-      <h2>Counter</h2>
-      <p>Current Count: {count}</p>
-      <button onClick={handleIncrement}>Increment</button>
-      <button onClick={handleDecrement}>Decrement</button>
+    <div className="container">
+      <div className="counter">
+        <h2>Current Count: {count}</h2>
+      </div>
+      <div className="button-row">
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <div className="button-row">
+        <button onClick={incrementIfOddHandler}>Increment If Odd</button>
+        <button onClick={asyncIncrementHandler}>Async Increment</button>
+      </div>
+      <div className="button-row">
+        <button className="timer-button" onClick={toggleTimerHandler}>
+          {timerRunning ? "Stop Timer" : "Start Timer"}
+        </button>
+      </div>
     </div>
   );
-};
+}
 
 export default Counter;
